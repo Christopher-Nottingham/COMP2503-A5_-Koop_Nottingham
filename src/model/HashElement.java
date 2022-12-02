b8e5e9eaa2b27d26eac066a6f577bcb3b32fe949
ethan <ethan@10.0.0.234>
2022-12-02 15:52:29 -0700

    Finished HashElement class (task 1)

--- src/model/HashElement.java
+++ src/model/HashElement.java
@@ -2,9 +2,31 @@ package model;
 
 public class HashElement {
 
-private String word; //key for the hash table
-private int count;
-
+	private String word; //key for the hash table
+	private int count;
+	
+	public HashElement (String word, int count) {
+		this.word = word;
+		this.count = count;
+	}
+	
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	@Override
+	public String toString() {
+		return "Number of times " + word + "occures is: " + count;
+	}
 
 
 }
