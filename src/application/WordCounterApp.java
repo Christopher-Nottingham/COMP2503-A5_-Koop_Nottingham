caa6ccbf09a7312dac35163deff9d0c97efa917a
Christopher-Nottingham <cnott729@mtroyal.ca>
2022-12-07 18:04:50 -0700

    Chris: Got push and all printing methods complete

--- src/application/WordCounterApp.java
+++ src/application/WordCounterApp.java
@@ -9,37 +9,32 @@ import model.*;
 public class WordCounterApp {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		WordCounter wc = new WordCounter(70);
-		HashElement hs = new HashElement("hi");
-		HashElement hs1 = new HashElement("hi");
-		wc.put(hs1);
-		wc.put(hs);
-		wc.printTable();
-		
-		
-		
-//		System.out.print("Enter the name of the desired file: ");
-//
-//		// res/argfoo.txt
-//		Scanner in = new Scanner(System.in);
-//		final String DB_PATH = in.nextLine();
-//		System.out.print("Enter the size of the table: ");
-//		int size = in.nextInt();
-//		final File database = new File(DB_PATH);
-//		Scanner wordReader = new Scanner(database);
-//		WordCounter wc = new WordCounter(size);
-//
-//		while (wordReader.hasNextLine()) {
-//			String lineOfWords = wordReader.nextLine();
-//			System.out.println(lineOfWords);
-//			String[] wordArray = lineOfWords.toLowerCase().split(" ");
-//			for (int index = 0; index < wordArray.length; index++) {
-//				// System.out.println(wordArray[index]);
-//				HashElement word = new HashElement(wordArray[index]);
-//				wc.put(word);
-//			}
-//
-//		}
 
+		System.out.print("Enter the name of the desired file: ");
+
+		Scanner in = new Scanner(System.in);
+		final String DB_PATH = in.nextLine();
+		System.out.print("Enter the size of the table: ");
+
+		int size = in.nextInt();
+
+		final File database = new File(DB_PATH);
+		Scanner wordReader = new Scanner(database);
+		WordCounter wc = new WordCounter(size);
+
+		while (wordReader.hasNextLine()) {
+			String lineOfWords = wordReader.nextLine();
+			String[] wordArray = lineOfWords.toLowerCase().trim().split(" ");
+			for (int index = 0; index < wordArray.length; index++) {
+				HashElement word = new HashElement(wordArray[index]);
+				wc.put(word);
+			}
+
+		}
+
+		wc.printTable();
+		wc.printDistinctWords();
+		wc.printMostComonWord();
+		wc.printTotalNumberOfWords();
 	}
 }
